30acfa4af55a15d2f33367dc094d9be9d958b0f7
Qingyuan Tian <qingyuantian2006@gmail.com>
2026-08-13 13:27:12 -0400

    pdfrreader

--- extractor/pdfreader.py
+++ extractor/pdfreader.py
@@ -5,3 +5,22 @@ class PDFReader:
         self.file_path = file_path
         self.doc = doc.open(file_path)
 
+    def get_page_count(self):
+        return len(self.doc)
+
+    def get_page(self, page_number):
+        return self.doc[page_number]
+        
+    def get_page_dimensions(self, page_number):
+        page = self.get_page(page_number)
+        return {
+            "width": page.rect.width,
+            "height": page.rect.height
+        }
+
+    def get_page_text(self, page_number):
+        page = self.get_page(page_number)
+        return page.get_text()
+
+    def close(self):       
+        self.doc.close()
